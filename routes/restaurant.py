c4c836512310823ca19e6a7a850a6fef68e7bf17
Humberto Suriano Medina <humbertosuriano24@gmail.com>
2022-07-26 22:52:07 -0500

    xd

--- routes/restaurant.py
+++ routes/restaurant.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from models.restaurant import restaurant
 
-from config.db import conn,session
+from config.db import conn
 
 restaurant_route = APIRouter()
 
@@ -13,6 +13,10 @@ def home():
 @restaurant_route.get('/restaurant')
 async def get_all_restaurant():
     try:
-        return await session.execute(restaurant.select()).fetchall()
+        all_restaurants = await conn.execute(restaurant.select()).fetchall()
+        if len(all_restaurants) > 0:
+            return {'restaurants' :all_restaurants}
+        else:
+            return {'restaurants': []}
     except Exception as e:
         return {'Error': str(e)}
